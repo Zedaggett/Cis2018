00adac37157d7e03cb54369218c4bc8b9e690c95
classroom <classroom@yccc.edu>
2018-09-05 19:30:59 -0400

    first commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,16 @@
+
+public class HelloWorld
+{
+	//An object is 2 things
+	//1.Actions. Methods.
+	//2 Attributes. Variables
+	private String name = "Zach";
+	
+	//main method. Starts the program
+	public static void main(String[] args)
+	{
+		//Prints to the console.
+		System.out.println("Hello World");
+		System.out.println("My first java app!!!");;
+	}
+}
